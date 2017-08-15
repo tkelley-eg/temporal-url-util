064b6d07f80d3fa6b3abfa0a32e0701e93aa0ed4
Troy Kelley <troy.kelley@e-gineering.com>
2017-08-15 09:15:59 -0400

    Initial commit

---
+++ src/main/java/com/egineering/temportalurlutil/TemportalUrlUtilApplication.java
@@ -0,0 +1,12 @@
+package com.egineering.temportalurlutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemportalUrlUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemportalUrlUtilApplication.class, args);
+	}
+}
